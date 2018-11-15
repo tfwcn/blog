b4e7ecd732fc44263862055005926b3171c82851
tfwcn@sina.cn <tfwcn@sina.cn>
2018-11-15 17:21:01 +0800

    更新到java 11版本运行

--- server/README.md
+++ server/README.md
@@ -1,1 +1,10 @@
 # blog
+
+## build jar文件
+```gradle build```
+
+## build失败时尝试升级gradle
+```scoop update gradle```
+
+## 启动服务
+```java -jar .\build\libs\blog-0.0.1.jar```
--- server/src/main/resources/youchat.sql
+++ server/src/main/resources/youchat.sql
@@ -1,167 +1,209 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat PostgreSQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50719
-Source Host           : localhost:3306
-Source Database       : youchat
+ Source Server         : localhost
+ Source Server Type    : PostgreSQL
+ Source Server Version : 110000
+ Source Host           : localhost:5432
+ Source Catalog        : blog
+ Source Schema         : public
 
-Target Server Type    : MYSQL
-Target Server Version : 50719
-File Encoding         : 65001
+ Target Server Type    : PostgreSQL
+ Target Server Version : 110000
+ File Encoding         : 65001
 
-Date: 2018-02-21 11:20:52
+ Date: 30/10/2018 23:08:15
 */
 
--- SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for error
+-- Table structure for errors
 -- ----------------------------
-DROP TABLE IF EXISTS "error";
-CREATE TABLE "error" (
-  "id" varchar(32) NOT NULL,
-  "num" bigint NOT NULL,
-  "message" varchar(200) DEFAULT NULL,
-  "detail" text,
-  "create_time" time NOT NULL,
-  "update_time" time DEFAULT NULL,
-  PRIMARY KEY ("id")
+DROP TABLE IF EXISTS "public"."errors";
+CREATE TABLE "public"."errors" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "num" int8 NOT NULL,
+  "message" varchar(200) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying,
+  "detail" text COLLATE "pg_catalog"."default",
+  "create_time" time(6) NOT NULL,
+  "update_time" time(6)
+)
+;
+COMMENT ON COLUMN "public"."errors"."id" IS 'GUID';
+COMMENT ON COLUMN "public"."errors"."num" IS '流水号';
+COMMENT ON COLUMN "public"."errors"."message" IS '错误信息';
+COMMENT ON COLUMN "public"."errors"."detail" IS '错误明细';
+COMMENT ON COLUMN "public"."errors"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."errors"."update_time" IS '最后更新时间';
+COMMENT ON TABLE "public"."errors" IS '错误信息';
+
+-- ----------------------------
+-- Table structure for notes
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."notes";
+CREATE TABLE "public"."notes" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "num" int8 NOT NULL,
+  "title" varchar(200) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying,
+  "content" text COLLATE "pg_catalog"."default",
+  "type_id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "create_time" time(6) NOT NULL,
+  "update_time" time(6)
+)
+;
+COMMENT ON COLUMN "public"."notes"."id" IS 'GUID';
+COMMENT ON COLUMN "public"."notes"."num" IS '流水号';
+COMMENT ON COLUMN "public"."notes"."title" IS '标题';
+COMMENT ON COLUMN "public"."notes"."content" IS '内容';
+COMMENT ON COLUMN "public"."notes"."type_id" IS '类型id';
+COMMENT ON COLUMN "public"."notes"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."notes"."update_time" IS '最后更新时间';
+COMMENT ON TABLE "public"."notes" IS '贴子信息';
+
+-- ----------------------------
+-- Table structure for notes_type
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."notes_type";
+CREATE TABLE "public"."notes_type" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "num" int8 NOT NULL,
+  "name" varchar(20) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying,
+  "detail" text COLLATE "pg_catalog"."default",
+  "create_time" time(6) NOT NULL,
+  "update_time" time(6),
+  "path" varchar(255) COLLATE "pg_catalog"."default"
+)
+;
+COMMENT ON COLUMN "public"."notes_type"."id" IS 'GUID';
+COMMENT ON COLUMN "public"."notes_type"."num" IS '流水号';
+COMMENT ON COLUMN "public"."notes_type"."name" IS '类型名称';
+COMMENT ON COLUMN "public"."notes_type"."detail" IS '类型描述';
+COMMENT ON COLUMN "public"."notes_type"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."notes_type"."update_time" IS '最后更新时间';
+COMMENT ON COLUMN "public"."notes_type"."path" IS '跳转地址';
+COMMENT ON TABLE "public"."notes_type" IS '贴子类型';
+
+-- ----------------------------
+-- Table structure for replies
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."replies";
+CREATE TABLE "public"."replies" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "num" int8 NOT NULL,
+  "user_id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "note_id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "reply_id" varchar(32) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying,
+  "content" text COLLATE "pg_catalog"."default",
+  "score" int8,
+  "create_time" time(6) NOT NULL,
+  "update_time" time(6)
+)
+;
+COMMENT ON COLUMN "public"."replies"."id" IS 'GUID';
+COMMENT ON COLUMN "public"."replies"."num" IS '流水号';
+COMMENT ON COLUMN "public"."replies"."user_id" IS '用户id';
+COMMENT ON COLUMN "public"."replies"."note_id" IS '贴子id';
+COMMENT ON COLUMN "public"."replies"."reply_id" IS '回复id';
+COMMENT ON COLUMN "public"."replies"."content" IS '内容';
+COMMENT ON COLUMN "public"."replies"."score" IS '分数';
+COMMENT ON COLUMN "public"."replies"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."replies"."update_time" IS '最后更新时间';
+COMMENT ON TABLE "public"."replies" IS '回复信息';
+
+-- ----------------------------
+-- Table structure for replies_score
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."replies_score";
+CREATE TABLE "public"."replies_score" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "num" int8 NOT NULL,
+  "user_id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "reply_id" varchar(32) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying,
+  "create_time" time(6) NOT NULL,
+  "update_time" time(6)
+)
+;
+COMMENT ON COLUMN "public"."replies_score"."id" IS 'GUID';
+COMMENT ON COLUMN "public"."replies_score"."num" IS '流水号';
+COMMENT ON COLUMN "public"."replies_score"."user_id" IS '用户id';
+COMMENT ON COLUMN "public"."replies_score"."reply_id" IS '回复id';
+COMMENT ON COLUMN "public"."replies_score"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."replies_score"."update_time" IS '最后更新时间';
+COMMENT ON TABLE "public"."replies_score" IS '回复评分记录';
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."users";
+CREATE TABLE "public"."users" (
+  "id" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "num" int8 NOT NULL,
+  "user_name" varchar(20) COLLATE "pg_catalog"."default" NOT NULL,
+  "login_name" varchar(50) COLLATE "pg_catalog"."default" NOT NULL,
+  "password" varchar(32) COLLATE "pg_catalog"."default" NOT NULL,
+  "create_time" time(6) NOT NULL,
+  "update_time" time(6)
+)
+;
+COMMENT ON COLUMN "public"."users"."id" IS 'GUID';
+COMMENT ON COLUMN "public"."users"."num" IS '流水号';
+COMMENT ON COLUMN "public"."users"."user_name" IS '用户名';
+COMMENT ON COLUMN "public"."users"."login_name" IS '登录账号';
+COMMENT ON COLUMN "public"."users"."password" IS '密码';
+COMMENT ON COLUMN "public"."users"."create_time" IS '创建时间';
+COMMENT ON COLUMN "public"."users"."update_time" IS '最后更新时间';
+COMMENT ON TABLE "public"."users" IS '用户信息';
+
+-- ----------------------------
+-- Primary Key structure for table errors
+-- ----------------------------
+ALTER TABLE "public"."errors" ADD CONSTRAINT "error_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table notes
+-- ----------------------------
+ALTER TABLE "public"."notes" ADD CONSTRAINT "note_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table notes_type
+-- ----------------------------
+ALTER TABLE "public"."notes_type" ADD CONSTRAINT "note_type_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table replies
+-- ----------------------------
+ALTER TABLE "public"."replies" ADD CONSTRAINT "reply_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table replies_score
+-- ----------------------------
+ALTER TABLE "public"."replies_score" ADD CONSTRAINT "reply_score_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Indexes structure for table users
+-- ----------------------------
+CREATE UNIQUE INDEX "users_login_name" ON "public"."users" USING btree (
+  "login_name" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
 );
 
-COMMENT ON TABLE "error" IS '错误信息';
-COMMENT ON COLUMN "error"."id" IS 'GUID';
-COMMENT ON COLUMN "error"."num" IS '流水号';
-COMMENT ON COLUMN "error"."message" IS '错误信息';
-COMMENT ON COLUMN "error"."detail" IS '错误明细';
-COMMENT ON COLUMN "error"."create_time" IS '创建时间';
-COMMENT ON COLUMN "error"."update_time" IS '最后更新时间';
+-- ----------------------------
+-- Primary Key structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
--- Table structure for note_type
+-- Foreign Keys structure for table notes
 -- ----------------------------
-DROP TABLE IF EXISTS "note_type";
-CREATE TABLE "note_type" (
-  "id" varchar(32) NOT NULL,
-  "num" bigint NOT NULL,
-  "name" varchar(20) DEFAULT NULL,
-  "detail" text,
-  "create_time" time NOT NULL,
-  "update_time" time DEFAULT NULL,
-  PRIMARY KEY ("id")
-);
-
-COMMENT ON TABLE "note_type" IS '贴子类型';
-COMMENT ON COLUMN "note_type"."id" IS 'GUID';
-COMMENT ON COLUMN "note_type"."num" IS '流水号';
-COMMENT ON COLUMN "note_type"."name" IS '类型名称';
-COMMENT ON COLUMN "note_type"."detail" IS '类型描述';
-COMMENT ON COLUMN "note_type"."create_time" IS '创建时间';
-COMMENT ON COLUMN "note_type"."update_time" IS '最后更新时间';
+ALTER TABLE "public"."notes" ADD CONSTRAINT "fk_note_type_id" FOREIGN KEY ("type_id") REFERENCES "public"."notes_type" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- ----------------------------
--- Table structure for user
+-- Foreign Keys structure for table replies
 -- ----------------------------
-DROP TABLE IF EXISTS "users";
-CREATE TABLE "users" (
-  "id" varchar(32) NOT NULL,
-  "num" bigint NOT NULL,
-  "user_name" varchar(20) NOT NULL,
-  "login_name" varchar(50) NOT NULL,
-  "password" varchar(32) NOT NULL,
-  "create_time" time NOT NULL,
-  "update_time" time DEFAULT NULL,
-  PRIMARY KEY ("id")
-);
-
-CREATE UNIQUE INDEX "users_login_name" ON "users" (
-  "login_name"
-);
-
-COMMENT ON TABLE "users" IS '用户信息';
-COMMENT ON COLUMN "users"."id" IS 'GUID';
-COMMENT ON COLUMN "users"."num" IS '流水号';
-COMMENT ON COLUMN "users"."user_name" IS '用户名';
-COMMENT ON COLUMN "users"."login_name" IS '登录账号';
-COMMENT ON COLUMN "users"."password" IS '密码';
-COMMENT ON COLUMN "users"."create_time" IS '创建时间';
-COMMENT ON COLUMN "users"."update_time" IS '最后更新时间';
+ALTER TABLE "public"."replies" ADD CONSTRAINT "fk_reply_note_id" FOREIGN KEY ("note_id") REFERENCES "public"."notes" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE "public"."replies" ADD CONSTRAINT "fk_reply_user_id" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- ----------------------------
--- Table structure for note
+-- Foreign Keys structure for table replies_score
 -- ----------------------------
-DROP TABLE IF EXISTS "note";
-CREATE TABLE "note" (
-  "id" varchar(32) NOT NULL,
-  "num" bigint NOT NULL,
-  "title" varchar(200) DEFAULT NULL,
-  "content" text,
-  "type_id" varchar(32) NOT NULL,
-  "create_time" time NOT NULL,
-  "update_time" time DEFAULT NULL,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "fk_note_type_id" FOREIGN KEY ("type_id") REFERENCES "note_type" ("id") ON UPDATE CASCADE
-);
-
-COMMENT ON TABLE "note" IS '贴子信息';
-COMMENT ON COLUMN "note"."id" IS 'GUID';
-COMMENT ON COLUMN "note"."num" IS '流水号';
-COMMENT ON COLUMN "note"."title" IS '标题';
-COMMENT ON COLUMN "note"."content" IS '内容';
-COMMENT ON COLUMN "note"."type_id" IS '类型id';
-COMMENT ON COLUMN "note"."create_time" IS '创建时间';
-COMMENT ON COLUMN "note"."update_time" IS '最后更新时间';
-
--- ----------------------------
--- Table structure for reply
--- ----------------------------
-DROP TABLE IF EXISTS "reply";
-CREATE TABLE "reply" (
-  "id" varchar(32) NOT NULL,
-  "num" bigint NOT NULL,
-  "user_id" varchar(32) NOT NULL,
-  "note_id" varchar(32) NOT NULL,
-  "reply_id" varchar(32) DEFAULT NULL,
-  "content" text,
-  "score" bigint DEFAULT NULL,
-  "create_time" time NOT NULL,
-  "update_time" time DEFAULT NULL,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "fk_reply_note_id" FOREIGN KEY ("note_id") REFERENCES "note" ("id") ON UPDATE CASCADE,
-  CONSTRAINT "fk_reply_user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON UPDATE CASCADE
-);
-
-COMMENT ON TABLE "reply" IS '回复信息';
-COMMENT ON COLUMN "reply"."id" IS 'GUID';
-COMMENT ON COLUMN "reply"."num" IS '流水号';
-COMMENT ON COLUMN "reply"."user_id" IS '用户id';
-COMMENT ON COLUMN "reply"."note_id" IS '贴子id';
-COMMENT ON COLUMN "reply"."reply_id" IS '回复id';
-COMMENT ON COLUMN "reply"."content" IS '内容';
-COMMENT ON COLUMN "reply"."score" IS '分数';
-COMMENT ON COLUMN "reply"."create_time" IS '创建时间';
-COMMENT ON COLUMN "reply"."update_time" IS '最后更新时间';
-
--- ----------------------------
--- Table structure for reply_score
--- ----------------------------
-DROP TABLE IF EXISTS "reply_score";
-CREATE TABLE "reply_score" (
-  "id" varchar(32) NOT NULL,
-  "num" bigint NOT NULL,
-  "user_id" varchar(32) NOT NULL,
-  "reply_id" varchar(32) DEFAULT NULL,
-  "create_time" time NOT NULL,
-  "update_time" time DEFAULT NULL,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "reply_score_reply_id" FOREIGN KEY ("reply_id") REFERENCES "reply" ("id") ON UPDATE CASCADE,
-  CONSTRAINT "reply_score_user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON UPDATE CASCADE
-);
-
-COMMENT ON TABLE "reply_score" IS '回复评分记录';
-COMMENT ON COLUMN "reply_score"."id" IS 'GUID';
-COMMENT ON COLUMN "reply_score"."num" IS '流水号';
-COMMENT ON COLUMN "reply_score"."user_id" IS '用户id';
-COMMENT ON COLUMN "reply_score"."reply_id" IS '回复id';
-COMMENT ON COLUMN "reply_score"."create_time" IS '创建时间';
-COMMENT ON COLUMN "reply_score"."update_time" IS '最后更新时间';
+ALTER TABLE "public"."replies_score" ADD CONSTRAINT "reply_score_reply_id" FOREIGN KEY ("reply_id") REFERENCES "public"."replies" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE "public"."replies_score" ADD CONSTRAINT "reply_score_user_id" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
