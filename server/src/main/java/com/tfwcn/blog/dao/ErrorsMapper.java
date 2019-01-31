0157a532d87c18a7a03dfa6654457defb2d6e1bc
tfwcn@sina.cn <tfwcn@sina.cn>
2019-01-31 12:01:54 +0800

    更新gradle

--- server/src/main/java/com/tfwcn/blog/dao/ErrorsMapper.java
+++ server/src/main/java/com/tfwcn/blog/dao/ErrorsMapper.java
@@ -1,11 +1,8 @@
 package com.tfwcn.blog.dao;
 
 import com.tfwcn.blog.models.Errors;
-import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
 
-@Mapper
 public interface ErrorsMapper {
     int deleteByPrimaryKey(String id);
 
